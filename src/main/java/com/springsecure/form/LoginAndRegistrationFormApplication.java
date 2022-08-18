b11bff20efc1900b3237b028c918b674f73cb997
Ahmad Mujtaba <cadeta6@gmail.com>
2022-08-18 17:22:24 +0500

    ci{whole-project}:added new proj

---
+++ src/main/java/com/springsecure/form/LoginAndRegistrationFormApplication.java
@@ -0,0 +1,12 @@
+package com.springsecure.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndRegistrationFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAndRegistrationFormApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springsecure/form/LoginAndRegistrationFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecure.form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRegistrationFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
