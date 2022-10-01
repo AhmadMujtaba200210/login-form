99c4c0fa79071664e75063c9c941225996cf0eb0
Ahmad Mujtaba <cadeta6@gmail.com>
2022-10-01 12:41:49 +0500

    CustomAuthorizationFilter

---
+++ src/main/java/com/springsecure/form/filter/CustomAuthorizationFilter.java
@@ -0,0 +1,57 @@
+package com.springsecure.form.filter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static java.util.Arrays.stream;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+
+public class CustomAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if(request.getServletPath().equals("/login")){
+            filterChain.doFilter(request,response);
+        }else {
+            String authorizationHeader=request.getHeader(AUTHORIZATION);
+            if(authorizationHeader!=null&&authorizationHeader.startsWith("Bearer ")){
+                try {
+                    String token=authorizationHeader.substring("Bearer ".length());
+                    Algorithm algorithm=Algorithm.HMAC256("secret".getBytes());
+                    JWTVerifier jwtVerifier= JWT.require(algorithm).build();
+                    DecodedJWT decodedJWT=jwtVerifier.verify(token);
+                    String username=decodedJWT.getSubject();
+                    String [] roles=decodedJWT.getClaim("roles").asArray(String.class);
+                    Collection<SimpleGrantedAuthority> authorities=new ArrayList<>();
+                    stream(roles).forEach(role->{
+                        authorities.add(new SimpleGrantedAuthority(role));
+                    });
+                    UsernamePasswordAuthenticationToken authenticationToken=
+                            new UsernamePasswordAuthenticationToken(username,null,authorities);
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    filterChain.doFilter(request,response);
+                }catch (Exception ex){
+                    response.setHeader("Error",ex.getMessage());
+                    response.sendError(FORBIDDEN.value());
+                }
+            }else{
+                    filterChain.doFilter(request,response);
+            }
+        }
+    }
+}
